999fabde8b658d4d9f346550fd20f320de82fa7b
Dylan Howell <dylan@dylanmhowell.com>
2019-03-23 15:19:13 -0700

    Update gatsby-config.js
    update wordPressUrl: https://radrank.com

--- starter/gatsby-config.js
+++ starter/gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
     title: `Twenty Nineteen Starter`,
     description: `Gatsby starter site for Twenty Nineteen Gatsby Theme.`,
     author: `@muhsinlk`,
-    wordPressUrl: `http://dev-twenty-nineteen-gatsby-demo.pantheonsite.io`
+    wordPressUrl: `https://radrank.com`
   },
   __experimentalThemes: [
     {
       resolve: "gatsby-theme-twentynineteen",
       options: {
-        wordPressUrl: `http://dev-twenty-nineteen-gatsby-demo.pantheonsite.io`
+        wordPressUrl: `https://radrank.com`
       }
     }
   ]
